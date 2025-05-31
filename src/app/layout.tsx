32736e329d1181919697d749149e19dacd62457e
Abinanthan D <dabinanthan@gmail.com>
2025-05-31 04:18:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AppHeader>
    
    with the text content:
    
        Toggle SidebarFocus GenToggle theme
    
    Relevant files:
    
        - https://6000-firebase-studio-1748653599921.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    can we change the tab icon same image as the one on title bar, because it will look more matching than brain emoji

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧠</text></svg>" />
+        <link rel="icon" href="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20width=%2224%22%20height=%2224%22%20viewBox=%220%200%2024%2024%22%20fill=%22none%22%20stroke=%22%235F9EA0%22%20stroke-width=%222%22%20stroke-linecap=%22round%22%20stroke-linejoin=%22round%22%3E%3Cpath%20d=%22M12%202L2%207l10%205%2010-5-10-5z%22%3E%3C/path%3E%3Cpath%20d=%22M2%2017l10%205%2010-5%22%3E%3C/path%3E%3Cpath%20d=%22M2%2012l10%205%2010-5%22%3E%3C/path%3E%3C/svg%3E" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
